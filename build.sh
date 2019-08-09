c08e74cb404156c4cc77c0b6f950d5a42e5faf6d
Slavi Pantaleev <slavi@devture.com>
2019-08-09 11:07:36 +0300

    Use a more specific golang Docker image version

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ rm -f ${PWD}/email2matrix-server
 docker run --rm -ti \
 	-v  ${PWD}:/work \
 	-w /work \
-	golang:1.12.7 \
+	golang:1.12.7-buster \
 	sh -c 'go get -u -v github.com/ahmetb/govvv && go build -a -ldflags "$(govvv -flags)" email2matrix-server.go'
